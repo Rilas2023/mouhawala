e541df9e56738dad94af1ea19c6caa5ab84791a1
Rilas2023 <said.koudil@gmail.com>
2023-05-25 08:51:28 -0500

    conflict message from collaborator2

---
+++ src/conflict/Conflict.java
@@ -0,0 +1,15 @@
+package conflict;
+
+public class Conflict {
+    public static void main(String[] args) {
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+        //conflict from collaborator 2
+    }
+}
